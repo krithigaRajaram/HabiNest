86ce7593b956e11a04980c4270fff09cbfbe209a
krithiga <krithigar2020@gmail.com>
2025-04-21 23:53:06 +0530

    feat: Delete habits added

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Navbar from '../components/Navbar';
 import HabitForm from '../components/HabitForm';
+import { TrashIcon } from '@heroicons/react/24/outline';
 
 const Dashboard = () => {
   const [habits, setHabits] = useState([]);
@@ -24,7 +25,21 @@ const Dashboard = () => {
     };
     fetchHabits();
   }, []);
-
+  
+  const handleDelete = async (id) => {
+    try {
+      const token = localStorage.getItem('token');
+      await axios.delete(`http://localhost:5000/api/habits/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+  
+      setHabits(habits.filter((habit) => habit._id !== id));
+    } catch (err) {
+      console.error('Error deleting habit:', err);
+    }
+  };
   return (
     <div className="flex flex-col min-h-screen bg-amber-50">
       <Navbar onAddHabitClick={() => setShowForm(true)} />
@@ -47,15 +62,25 @@ const Dashboard = () => {
               className="p-4 rounded-lg shadow-md bg-white border"
               style={{ borderLeft: `6px solid ${habit.color}` }}
             >
-              <div className="flex items-center justify-between mb-2">
-                <div className="text-3xl">{habit.icon}</div>
               
-              </div>
               <h3 className="text-xl font-semibold text-amber-900">{habit.title}</h3>
               <p className="text-sm text-gray-600">{habit.description}</p>
               <span className="inline-block mt-2 text-xs font-medium bg-amber-100 text-amber-700 px-2 py-1 rounded">
                 {habit.frequency}
               </span>
+              <div className="flex items-center justify-between mb-2">
+              <div className="text-3xl">{habit.icon}</div>
+              <button
+                onClick={() => handleDelete(habit._id)}
+                className="text-black hover:scale-110 transition-transform duration-200"
+                title="Delete Habit"
+              >
+                <TrashIcon className="h-5 w-5" />
+              </button>
+            </div>
+
+
+
             </div>
           ))}
         </div>
